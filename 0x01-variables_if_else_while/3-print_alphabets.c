899a53d7822bb8073763b3c44d1f95b4f0eb08e9
leoncepromus <leopromus@gmail.com>
2022-09-09 15:50:44 -0700

    print _alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(void)
+
+{
+
+char ch;
+
+for (ch = 'a' ; ch <= 'z' ; ch++)
+
+{
+
+if (ch != 'e' && ch != 'q')
+
+{
+
+	putchar(ch);
+
+}
+
+}
+
+putchar('\n');
+
+return (0);
+
