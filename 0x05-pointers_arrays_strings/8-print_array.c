dd4c631722773f746c68f25583d5c076f155cde5
leoncepromus <leopromus@gmail.com>
2022-09-19 08:54:50 -0700

    print an element of an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,43 @@
+
+#include<stdio.h>                                    
+#include "main.h"                                    
+/**print n element of array */                       
+void print_array(int *a, int n)                      
+{                                                    
+int inc;                                             
+int inc;                                             
+for(inc =0; inc < n ; inc++)                         
+{                                                    
+if(inc != n -1)                                      
+printf(%d, *, a[inc]);                               
+else                                                 
+printf("%d", a[inc]);                                
+}                 #include<stdio.h>                                    
+
+#include "main.h"                                    
+
+/**print n element of array */                       
+
+void print_array(int *a, int n)                      
+
+{                                                    
+
+int inc;                                             
+
+int inc;                                             
+
+for(inc =0; inc < n ; inc++)                         
+
+{                                                    
+
+if(inc != n -1)                                      
+
+printf(%d, *, a[inc]);                               
+
+else                                                 
+
+printf("%d", a[inc]);                                
+
+}       
+putchar(10);
+}
