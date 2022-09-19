16d8ead2bceccdd2fd990b79bce5b9287e394c51
leoncepromus <leopromus@gmail.com>
2022-09-19 08:42:10 -0700

    printsevery character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/* print every character */
+void puts2(char *str)
+{
+int i=0;
+while (*(str + i ) != '\0')
+{
+if (i % 2 == 00
+putchat(*(str +i));
+i++;
+}
+putchar(10);
