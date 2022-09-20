dc0956126b233cc1f2f3cf08d29eceabc9b580fe
leoncepromus <leopromus@gmail.com>
2022-09-20 10:22:34 -0700

    this a half program

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,11 @@
+
+#include<stdio.h>                                                                                            
+#include<string.h>                                                                                           
+#include "main.h"   #include<stdio.h>                                                                                            
+
+#include<string.h>                                                                                           
+
+#include "main.h" 
+void puts_half(char *str){
+string str = "this is a half program";
+divide(str,2);
