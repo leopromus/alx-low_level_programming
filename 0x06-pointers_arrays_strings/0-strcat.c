376f58b11fcc091b4ee02d2610cb7d08d5ffbab5
leoncepromus <leopromus@gmail.com>
2022-09-21 06:27:14 -0700

    0-strconcetenates

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+// function to concatenate two strings
+  
+char *_strcat(char *dest,char *src)
+{
+	int index =0,  dest_len =0;
+	while (dest[index++])
+		dest_len++;
+	for(index = 0; src[index];index++)
+		dest[dest_len++] = src[index];
+	return (dest);
+}
